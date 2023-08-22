9fbd1d27cdec72aecaa1c9d4716a8be4246503c3
mkwwd <95129758+mkwwd@users.noreply.github.com>
2023-08-22 13:58:56 +0900

    [D4] Title: 서로소 집합, Time: 448 ms, Memory: 105,040 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int parents[], N;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			
+			st = new StringTokenizer(br.readLine());
+			sb = new StringBuilder();
+			
+			sb.append("#").append(test_case).append(" ");
+			
+			N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			parents = new int[N+1];
+			
+			for(int i=0; i<=N; i++) {
+				parents[i] = i;
+			}
+			
+			for(int i=0; i<M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int input = Integer.parseInt(st.nextToken());
+				if(input == 0) {
+					int a = Integer.parseInt(st.nextToken());
+					int b = Integer.parseInt(st.nextToken());
+					union(a, b);
+				}else {
+					int a = Integer.parseInt(st.nextToken());
+					int b = Integer.parseInt(st.nextToken());
+					if(find(a) == find(b)) {
+						sb.append("1");
+					}
+					else { 
+						sb.append("0");
+					}
+				}
+				
+			}	
+			
+			System.out.println(sb);
+		}
+		
+	}
+
+	private static int find(int a) {
+		if(a == parents[a]) return a;
+		return parents[a] = find(parents[a]);
+	}
+
+	private static boolean union(int a, int b) {
+		
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if(aRoot == bRoot) return false;
+		parents[bRoot] = aRoot;
+		return true;
+	}
+}
+
