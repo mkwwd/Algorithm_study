7aa050bfcbe4c937c788323fca4bac371bf7f128
mkwwd <95129758+mkwwd@users.noreply.github.com>
2024-07-08 03:40:02 +0900

    [Gold V] Title: Coins, Time: 76 ms, Memory: 12016 KB -BaekjoonHub

--- 백준/Gold/3067. Coins/Coins.java
+++ 백준/Gold/3067. Coins/Coins.java
@@ -17,26 +17,20 @@ public class Main {
             int N = Integer.parseInt(br.readLine());
             int coin[] = new int[N+1];
             st = new StringTokenizer(br.readLine());
-            for(int j=1; j<N+1; j++){
+            for(int j=0; j<N; j++){
                 coin[j] = Integer.parseInt(st.nextToken());
             }
 
             int P = Integer.parseInt(br.readLine());
-            int dp[][] = new int[N+1][P+1];
-            for(int j=1; j<N+1; j++){
-                for(int k=1; k<P+1; k++){
-                    int gap = k-coin[j];
-                    if(gap == 0){
-                        dp[j][k] = dp[j-1][k] + 1;
-                    }else if(gap > 0 && dp[j][gap] != 0){
-                        dp[j][k] = dp[j-1][k] + dp[j][gap];
-                    }else{
-                        dp[j][k] = dp[j-1][k];
-                    }
+            int dp[] = new int[P+1];
+            dp[0] = 1;
+            for(int j=0; j<N; j++){
+                for(int k=coin[j]; k<P+1; k++){
+                    dp[k] += dp[k - coin[j]];
                 }
             }
 
-            sb.append(dp[N][P] + "\n");
+            sb.append(dp[P] + "\n");
 
         }
 
