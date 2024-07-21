5155e3c52c714fd55910544b5ed849f12ed0b5ce
mkwwd <95129758+mkwwd@users.noreply.github.com>
2024-07-21 23:53:18 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 156 ms, Memory: 19796 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        int div = 1000000009;
+
+        int dp[][] = new int[1001][1001];
+
+        dp[1][1] = 1;
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 2;
+        dp[3][3] = 1;
+
+        for(int i=4; i<1001; i++){
+            for(int j=2; j<=i; j++){
+                dp[i][j] = ((dp[i-1][j-1] + dp[i-2][j-1])%div + dp[i-3][j-1])%div;
+            }
+        }
+
+        for(int i=0; i<T; i++){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            long ans = 0;
+            for(int j=0; j<=m; j++){
+                ans = (ans + dp[n][j])%div;
+            }
+            sb.append(ans+"\n");
+
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
