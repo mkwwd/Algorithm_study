3b4f2bc661ab8d84033a6ac14c3371d2c68b524f
mkwwd <95129758+mkwwd@users.noreply.github.com>
2024-11-13 02:55:31 +0900

    [level 1] Title: 모의고사, Time: 1.00 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -6,21 +6,22 @@ class Solution {
         int[] stu1 = {1,2,3,4,5};
         int[] stu2 = {2,1,2,3,2,4,2,5};
         int[] stu3 = {3,3,1,1,2,2,4,4,5,5};
-        int[] ans = new int[3];
-        int max = 0;
+        
+        int ans1 = 0;
+        int ans2 = 0;
+        int ans3 = 0; 
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == stu1[i%stu1.length]) ans[0] ++;
-            if(answers[i] == stu2[i%stu2.length]) ans[1] ++;
-            if(answers[i] == stu3[i%stu3.length]) ans[2] ++;
-            max = Math.max(ans[0], Math.max(ans[1], ans[2]));
+            if(answers[i] == stu1[i%stu1.length]) ans1 ++;
+            if(answers[i] == stu2[i%stu2.length]) ans2 ++;
+            if(answers[i] == stu3[i%stu3.length]) ans3 ++;
         }
         
-        for(int i=0; i<3; i++){
-           if(ans[i] == max){
-               ansList.add(i+1);
-           }
-        }
+        int max = Math.max(ans1, Math.max(ans2, ans3));
+        
+        if(max == ans1) ansList.add(1);
+        if(max == ans2) ansList.add(2);
+        if(max == ans3) ansList.add(3);
         
         int result[] = new int[ansList.size()];
         
