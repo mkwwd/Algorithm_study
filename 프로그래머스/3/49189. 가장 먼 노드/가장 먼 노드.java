3bfa871228aca2ca6a9b4eca63010dad62a95286
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-05-12 18:21:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 29.41 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -3,35 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
         
-        int dept[] = new int[n+1];
-        ArrayList<ArrayList<Integer>> info = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> node = new ArrayList<>();
         
         for(int i=0; i<n+1; i++){
-            info.add(new ArrayList<>());
+            node.add(new ArrayList<>());
         }
         
         for(int i=0; i<edge.length; i++){
             int from = edge[i][0];
             int to = edge[i][1];
-            info.get(from).add(to);
-            info.get(to).add(from);
+            node.get(from).add(to);
+            node.get(to).add(from);
         }
         
-        Deque<Integer> node = new ArrayDeque<>();
         boolean visited[] = new boolean[n+1];
-        
-        node.add(1);
+            
+        Deque<Integer> que = new ArrayDeque<>();
+        que.add(1);
         visited[1] = true;
         int size = 0;
-        
-        while(!node.isEmpty()){
-            size = node.size();
+            
+        while(!que.isEmpty()){
+            size = que.size();
             for(int i=0; i<size; i++){
-                int now = node.poll();
-                for(int a : info.get(now)){
-                    if(visited[a]) continue;
-                    visited[a] = true;
-                    node.add(a);
+                int now = que.poll();
+                for(int next : node.get(now)){
+                    if(visited[next]) continue;
+                    visited[next] = true;
+                    que.add(next);
                 }
             }
         }
