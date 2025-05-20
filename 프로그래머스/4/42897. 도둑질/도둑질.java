a0a76595bddf974a536798fe57dedc4db506229c
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-05-21 04:16:39 +0900

    [level 4] Title: 도둑질, Time: 29.01 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,19 +1,21 @@
 class Solution {
     public int solution(int[] money) {
         
-        int[] getFirst = new int[money.length+1];
-        int[] getLast = new int[money.length+1];
+        int getFirst[] = new int[money.length];
+        int getLast[] = new int[money.length];
         
-        for(int i=0; i<money.length-1; i++){
-            getFirst[i+1] = money[i];
-            getLast[i+1] = money[i+1];
+        getFirst[0] = money[0];
+        getFirst[1] = Math.max(money[0], money[1]);
+        
+        getLast[0] = money[1];
+        getLast[1] = Math.max(money[1], money[2]);
+        
+        for(int i=2; i<money.length-1; i++){
+            getFirst[i] = Math.max(money[i] + getFirst[i-2], getFirst[i-1]);
+            getLast[i] = Math.max(money[i+1] + getLast[i-2], getLast[i-1]);
         }
         
-        for(int i=2; i<=money.length; i++){
-            getFirst[i] = Math.max(getFirst[i] + getFirst[i-2], getFirst[i-1]);
-            getLast[i] = Math.max(getLast[i] + getLast[i-2], getLast[i-1]);
-        }
+        return Math.max(getFirst[money.length-2], getLast[money.length-2]);
         
-        return Math.max(getFirst[money.length], getLast[money.length]);
     }
 }
