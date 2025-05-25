dd323b8027f3e34986dfdb0177c3826453c5e303
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-05-26 03:31:46 +0900

    [level 3] Title: 단속카메라, Time: 9.63 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -5,25 +5,23 @@ class Solution {
         
         Arrays.sort(routes, new Comparator<int[]>(){
             @Override
-            public int compare(int[] o1, int[] o2){
-                if(o1[1] == o2[1]){
-                    return o1[0] - o2[0];
+            public int compare(int o1[], int o2[]){
+                if(o1[1]==o2[1]){
+                    return o1[0]-o2[0];
                 }
-                return o1[1] - o2[1];
+                return o1[1]-o2[1];
             }
         });
         
-        int start = routes[0][1];
-        int answer = 1;
+        int cnt = 1;
+        int cctv = routes[0][1];
+        
         for(int i=0; i<routes.length; i++){
-            if(routes[i][0] <= start && start <= routes[i][1]){
-                continue;
-            }else{
-                start = routes[i][1];
-                answer ++;
-            }
+            if(routes[i][0]<=cctv && cctv <= routes[i][1]) continue;
+            cnt++;
+            cctv = routes[i][1];
         }
-      
-        return answer;
+            
+        return cnt;
     }
 }
