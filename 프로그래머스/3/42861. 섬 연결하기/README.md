a8c8e9671aea1183bc1abe6fdef6a929d3e9c010
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-05-26 21:03:46 +0900

    [level 3] Title: 섬 연결하기, Time: 2.01 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
 
 ### 성능 요약
 
-메모리: 82.7 MB, 시간: 1.55 ms
+메모리: 87.4 MB, 시간: 2.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 02일 14:19:53
+2025년 05월 26일 21:03:42
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -9,36 +9,36 @@ class Solution {
         Arrays.sort(costs, new Comparator<int[]>(){
             @Override
             public int compare(int o1[], int o2[]){
-                return o1[2] - o2[2];
+                return o1[2]-o2[2];
             }
         });
         
-        int answer = 0;
-        parent = new int [n];
-        for(int i=0; i<n; i++){
+        parent = new int[n];
+        for(int i=0; i<parent.length; i++){
             parent[i] = i;
         }
         
+        int sum = 0;
+        
         for(int i=0; i<costs.length; i++){
-            if(find(costs[i][0]) != find(costs[i][1])){
-                answer += costs[i][2];
-                union(costs[i][0], costs[i][1]);
-            }
+            if(find(costs[i][0]) == find(costs[i][1])) continue;
+            sum += costs[i][2];
+            union(costs[i][0], costs[i][1]);
         }
         
-        return answer;
-    }
-    
-    public int find(int now){
-        if(parent[now] == now) return now;
-        else return find(parent[now]);
+        return sum;
     }
     
     public void union(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a != b){
-            parent[b] = a;
-        }
+        int pa = parent[a];
+        int pb = parent[b];
+        if(pa != pb) parent[pb] = pa;
     }
+    
+    public int find(int x){
+        if(parent[x] == x) return x;
+        else return parent[x] = find(parent[x]);
+    }
+    
+
 }
