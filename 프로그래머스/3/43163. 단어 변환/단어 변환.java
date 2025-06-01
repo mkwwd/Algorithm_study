be13a6d0306bf69b1b25c1d3ae692e262d87c8e1
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-06-02 04:50:41 +0900

    [level 3] Title: 단어 변환, Time: 14.60 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    
+    static int min = Integer.MAX_VALUE;
+    
+    public int solution(String begin, String target, String[] words) {
+        
+        int answer = 0;
+        boolean use[] = new boolean[words.length];
+        
+        dfs(begin, target, words, use, 0);
+        
+        if(min == Integer.MAX_VALUE){
+            min = 0;
+        } 
+        
+        return min;
+    }
+    
+    public void dfs(String begin, String target, String[] words, boolean[] use, int sum){
+        
+        System.out.println(begin + target + sum);
+        
+        if(sum > words.length || sum > min) return;
+        
+        if(begin.equals(target)){
+            min = Math.min(min, sum);
+            return;
+        }
+        
+        for(int i=0; i<words.length; i++){
+            if(use[i]) continue;
+            int cnt = 0;
+            for(int j=0; j<begin.length(); j++){
+                if(begin.charAt(j) != words[i].charAt(j)) cnt++;
+            }
+           
+            if(cnt == 1){
+                use[i] = true;
+                dfs(words[i], target, words, use, sum+1);
+                use[i] = false;
+            }
+            
+        }
+        
+    }
+    
+}
