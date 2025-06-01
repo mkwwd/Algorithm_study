3d7ba773713cd7defd210f3fbc56ab98ca54c38f
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-06-02 03:39:00 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,30 @@
 class Solution {
     
-    static boolean connect[];
+    static boolean visited[];
     
     public int solution(int n, int[][] computers) {
         
-        connect = new boolean [n];
         int answer = 0;
+        visited = new boolean[n];
         
         for(int i=0; i<n; i++){
-            if(!connect[i]){
-                answer++;
-                network(i, computers);
-            }
+            if(visited[i]) continue;
+            answer++;
+            connect(i, computers);
         }
-        
+         
         return answer;
     }
     
-    public void network(int start, int[][] arr){
-        for(int i=0; i<arr.length; i++){
-            if(!connect[i] && arr[start][i] == 1 && start != i){
-                connect[i] = true;
-                network(i, arr);
+    public void connect(int st, int[][] computers){
+        
+        for(int i=0; i<computers.length; i++){
+            if(computers[st][i] == 1 && !visited[i]){
+                visited[i] = true;
+                connect(i, computers);
             }
         }
+        
     }
+
 }
