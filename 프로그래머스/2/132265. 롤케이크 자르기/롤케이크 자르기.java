ce064bd78a2e0acd8db55e2df86fdc7d16c2d736
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-06-06 02:56:29 +0900

    [level 2] Title: 롤케이크 자르기, Time: 220.72 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -7,23 +7,24 @@ class Solution {
         HashMap<Integer, Integer> b = new HashMap<>();
         
         for(int i=0; i<topping.length; i++){
-            b.put(topping[i], b.getOrDefault(topping[i], 0)+1);
+            b.put(topping[i], b.getOrDefault(topping[i], 0) + 1);
         }
         
-        int answer = 0;
+        int cnt = 0;
         
-        for(int i=0; i<topping.length-1; i++){
-            int now =topping[i];
-            a.put(now, a.getOrDefault(now, 0)+1);
-            if(b.getOrDefault(now, 0) == 1){
-                b.remove(now);
+        for(int i=0; i<topping.length; i++){
+            a.put(topping[i], a.getOrDefault(topping[i], 0) + 1);
+            int out = b.getOrDefault(topping[i], 0);
+            if(out == 1){
+                b.remove(topping[i]);
             }else{
-                b.put(now, b.getOrDefault(now, 0)-1);
+                b.put(topping[i], out-1);
             }
             
-            if(a.size() == b.size()) answer++;
-        } 
+            if(a.size() == b.size()) cnt++;
+        }
         
-        return answer;
+        return cnt;
+        
     }
 }
