6be47b425177b7a32304df8fb457b18de4cdacc6
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-06-13 05:26:41 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey !=0){
+            int n = storey%10;
+            storey /= 10;
+            
+            if(n == 0) continue;
+            
+            if(n > 5){
+                answer += 10-n;
+                storey++;
+            }else if(n < 5){
+                answer += n;
+            }else{
+                answer += 5;
+                if(storey % 10 >= 5) storey ++;
+            }
+        }
+        
+        return answer;
+    }
+}
