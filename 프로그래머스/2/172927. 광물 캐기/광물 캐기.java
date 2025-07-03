56ae08858b1f0efc3b88b779dd69c6440cf628cb
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-07-04 06:21:29 +0900

    [level 2] Title: 광물 캐기, Time: 1.21 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,59 +1,67 @@
 import java.util.*;
 
 class Solution {
-
+    
     static int tired[][] = {{1, 1, 1},{5, 1, 1},{25, 5, 1}};
     
     public int solution(int[] picks, String[] minerals) {
         
-        int maxBundle = Math.min((picks[0] + picks[1] + picks[2]), (minerals.length + 4) / 5);
-        
-        List<int[]> bundles = new ArrayList<>();
-        
-        for(int i = 0; i < maxBundle; i++) {
-            int[] count = new int[3];
-            for(int j = 0; j < 5; j++) {
-                int idx = i * 5 + j;
-                if(idx >= minerals.length) break;
-                if(minerals[idx].equals("diamond")) count[0]++;
-                else if(minerals[idx].equals("iron")) count[1]++;
-                else count[2]++;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                if(o2[0] != o1[0]) return o2[0] - o1[0];
+                if(o2[1] != o1[1]) return o2[1] - o1[1];
+                return o2[2] - o1[2];
             }
-            bundles.add(count);
-        }
-        
-        // 피로도가 큰 묶음부터 정렬
-        bundles.sort((o1, o2) -> {
-            int score1 = o1[0] * 25 + o1[1] * 5 + o1[2];
-            int score2 = o2[0] * 25 + o2[1] * 5 + o2[2];
-            return score2 - score1;
         });
+        
+        int size = Math.min((int)Math.ceil((double)minerals.length/5), (picks[0] + picks[1] + picks[2]));
+            
+        for(int i=0; i<minerals.length;){
+            if(pq.size() == size) break;
+            int[] count = new int[3];
+            for(int j=0; j<5; j++){
+                if(i+j >= minerals.length) break;
+                if(minerals[i+j].equals("diamond")){
+                    count[0]++;
+                }else if(minerals[i+j].equals("iron")){
+                    count[1]++;
+                }else{
+                    count[2]++;
+                }
+            }
+            pq.add(count);
+            i += 5;
+        };
         
         int answer = 0;
         
-        for(int[] bundle : bundles){
-            int pick = -1;
+        while(!pq.isEmpty()){
+            
             if(picks[0] > 0){
-                pick = 0; picks[0]--;
+                answer += breakMinerals(0, pq.poll());
+                picks[0]--;
             }else if(picks[1] > 0){
-                pick = 1; picks[1]--;
+                answer += breakMinerals(1, pq.poll());
+                picks[1]--;
             }else if(picks[2] > 0){
-                pick = 2; picks[2]--;
-            }
-            
-            if(pick == -1) break;
-            
-            answer += breakMinerals(pick, bundle);
+                answer += breakMinerals(2, pq.poll());
+                picks[2]--;
+            }else break;
         }
-        
+           
         return answer;
     }
     
     public int breakMinerals(int pick, int[] minerals){
+        
         int cnt = 0;
-        for(int i = 0; i < 3; i++) {
+        
+        for(int i=0; i<minerals.length; i++){
             cnt += tired[pick][i] * minerals[i];
-        }
+        } 
+        
         return cnt;
     }
+    
 }
