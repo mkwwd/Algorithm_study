e39ab5d9f8e26d9e64f3cd6bf3d7abd700623b17
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-07-11 00:31:01 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 36.68 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -2,14 +2,14 @@ function solution(r1, r2) {
     
     var answer = 0;
     
-    for(let i=1; i<r2; i++){
+    for(let i=1; i<=r2; i++){
         var max = Math.floor(Math.sqrt(Math.pow(r2,2) - Math.pow(i,2)))
-        var min = Math.ceil(Math.sqrt(Math.pow(r1,2) - Math.pow(i, 2)))
-        answer += max - min+1;
+        var min = Math.ceil(Math.sqrt(Math.pow(r1,2) - Math.pow(i, 2))) || 0
+        var cnt = max - min + 1;
+        answer += cnt*4;
     }
     
-    answer *= 4;
-    answer += 4;
+    //answer += 4;
    
     return answer;
 }
