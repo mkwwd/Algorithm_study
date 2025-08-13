af24f617765041a4fb351ec7e3bf2caa00d38705
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-08-13 12:18:28 +0900

    [level 2] Title: 가장 큰 수, Time: 2921.86 ms, Memory: 388 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
 
 ### 성능 요약
 
-메모리: 413 MB, 시간: 2902.93 ms
+메모리: 388 MB, 시간: 2921.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 15일 23:32:03
+2025년 08월 13일 12:18:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,18 +4,17 @@ class Solution {
     
     public String solution(int[] numbers) {
         
-        String num[] = new String[numbers.length];
+        String num[] = new String [numbers.length];
         
-        for(int i=0; i<numbers.length; i++){
-            num[i] = numbers[i]+"";
+        for(int i=0; i<num.length; i++){
+            num[i] = numbers[i] + "";
         }
         
-        Arrays.sort(num, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(num, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+        
         String answer = "";
         
-        if (num[0].equals("0")) {
-            return "0";
-        }
+        if(num[0].equals("0")) return "0";
         
         for(int i=0; i<num.length; i++){
             answer = answer + num[i];
