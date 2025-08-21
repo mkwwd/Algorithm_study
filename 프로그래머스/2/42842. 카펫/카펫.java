05646278624367ee50ed6c1384c44aff810958f8
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-08-22 03:47:17 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,17 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         
-        int[] answer = new int[2];
+        int answer[] = new int[2];
         
-        for(int i=1; i<=(int) Math.sqrt(yellow); i++){
-            if(yellow%i == 0){
-                int sum = (yellow/i + i + 2) * 2;
-                if(sum == brown){
-                    answer[0] = yellow/i + 2;
-                    answer[1] = i + 2;
-                    break;
-                }
-            }
+        for(int i=1; i<=(int)Math.sqrt(yellow); i++){       
+            if(yellow%i != 0) continue;
+            int width = yellow/i + 2;
+            int height = i+2;
+            int brownCnt = width*height - yellow;
+            if(brownCnt == brown){
+                answer[0] = width;
+                answer[1] = height;
+                break;
+            } 
         }
      
         return answer;
