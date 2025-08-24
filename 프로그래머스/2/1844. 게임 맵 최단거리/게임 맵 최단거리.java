22132511e345d880a68187602fccfffb74831bc6
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-08-25 01:42:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.92 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -8,41 +8,35 @@ class Solution {
         
         int N = maps.length;
         int M = maps[0].length;
-        
         boolean visited[][] = new boolean[N][M];
-        visited[0][0] = true; 
+        visited[0][0] = true;
         
-        Deque<int[]> que = new ArrayDeque<>();
-        que.add(new int[]{0,0,1});
+        ArrayDeque<int[]> que = new ArrayDeque<>();
+        que.add(new int[]{0, 0, 1});
         
         int answer = Integer.MAX_VALUE;
         
         while(!que.isEmpty()){
             int now[] = que.poll();
-            int nowX = now[0];
-            int nowY = now[1];
-            int cnt = now[2];
             for(int i=0; i<4; i++){
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
+                int nextX = now[0]+dx[i];
+                int nextY = now[1]+dy[i];
                 if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
                 if(maps[nextX][nextY] == 0) continue;
                 if(visited[nextX][nextY]) continue;
-                // 배열안이면서 벽이아니고 방문한적이 없음 => 방문가능
                 visited[nextX][nextY] = true;
-                // 마지막인지 체크
                 if(nextX == N-1 && nextY == M-1){
-                    answer = Math.min(answer, cnt+1);
-                    continue;
+                    answer = now[2]+1;
+                    break;
                 }
-                que.add(new int[]{nextX, nextY, cnt+1});
+                que.add(new int[]{nextX, nextY, now[2]+1});
             }
         }
         
         if(answer == Integer.MAX_VALUE){
             answer = -1;
         }
-        
+
         return answer;
     }
 }
