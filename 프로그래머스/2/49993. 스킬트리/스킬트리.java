720f2c695f5aeb93ef96d36feaf2c1951841848e
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-09-19 03:45:13 +0900

    [level 2] Title: 스킬트리, Time: 0.27 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -3,31 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         
-        ArrayList<Character> str = new ArrayList<>();
+        ArrayList<Character> turn = new ArrayList<>();
         
         for(int i=0; i<skill.length(); i++){
-            str.add(skill.charAt(i));
+            turn.add(skill.charAt(i));
         }
-        
+            
         int answer = 0;
         
         for(int i=0; i<skill_trees.length; i++){
-            String trees = skill_trees[i];
             int index = 0;
-            boolean isPossible = true;
-            for(int j=0; j<trees.length(); j++){
-                char now = trees.charAt(j);
-                if(!str.contains(now)) continue;
-                if(str.indexOf(now) == index){
-                    index ++;
-                }else{
-                    isPossible = false;
-                    break;
+            String now = skill_trees[i];
+            boolean possible = true;
+            for(int j=0; j<now.length(); j++){
+                if(turn.contains(now.charAt(j))){
+                    if(turn.get(index) == (now.charAt(j))){
+                        index++;
+                    }else{
+                        possible = false;
+                        break;
+                    }
                 }
             }
-            
-            if(isPossible) answer++;
-            
+            if(possible) answer++;
         }
         
         return answer;
