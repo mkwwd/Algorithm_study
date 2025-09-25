64bef3471d7e03fb3843faabcb296127f2849d60
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-09-26 01:45:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.15 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,18 @@
 class Solution {
     public long solution(int n) {
-    
-        long dp[] = new long[n+1];
         
-        if(n == 1) return 1;
+        long dp[];
+    
+        if(n < 3){
+            dp = new long[3];
+        }else{
+            dp = new long[n+1];
+        }        
         
         dp[1] = 1;
         dp[2] = 2;
         
-        for(int i=3; i<n+1; i++){
+        for(int i=3; i<=n; i++){
             dp[i] = (dp[i-2] + dp[i-1])%1234567;
         }
         
