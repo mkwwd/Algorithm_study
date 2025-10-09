8b1840d6b015e448475cd1a30ac9d7eef144da74
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-10-10 03:45:26 +0800

    [level 2] Title: 두 큐 합 같게 만들기, Time: 40.77 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -10,35 +10,34 @@ class Solution {
         long sum2 = 0;
         
         for(int i=0; i<queue1.length; i++){
-            que1.add(queue1[i]);
-            que2.add(queue2[i]);
             sum1 += queue1[i];
+            que1.add(queue1[i]);
             sum2 += queue2[i];
+            que2.add(queue2[i]);
         }
         
         if(sum1 == sum2) return 0;
         
-        int size = queue1.length * 3;
         int cnt = 0;
+        int size = que1.size()*2+1;
         
         while(cnt <= size && !que1.isEmpty() && !que2.isEmpty()){
-        
             if(sum1 > sum2){
-                int poll = que1.pollFirst();
-                que2.add(poll);
-                sum1 -= poll;
-                sum2 += poll;
-            }else{
-                int poll = que2.pollFirst();
-                que1.add(poll);
-                sum1 += poll;
-                sum2 -= poll;
+                int out = que1.poll();
+                sum1 -= out;
+                que2.add(out);
+                sum2 += out;
+            }else {
+                int out = que2.poll();
+                sum2 -= out;
+                que1.add(out);
+                sum1 += out;
             }
             
             cnt++;
             if(sum1 == sum2) return cnt;
-            
         }
+        
         
         return -1;
     }
