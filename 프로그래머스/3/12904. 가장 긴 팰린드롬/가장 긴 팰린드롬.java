b5eb508bb26fe1feb070e8815c02ad65dff478c5
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-11-13 00:28:57 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 48.64 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -4,25 +4,19 @@ class Solution
 {
     public int solution(String s)
     {
-        int len = s.length();
-        int palindrome[][] = new int[len][len];
         int answer = 1;
+        int N = s.length();
+        int palindrome[][] = new int[N][N];
         
-        for(int i=0; i<len; i++){
+        for(int i=0; i<N; i++){
             for(int j=0; j<i+1; j++){
-                if(i == j){
-                    palindrome[j][i] = 1;
-                }else{
-                    if(s.charAt(j) != s.charAt(i)) continue;
-                    if(j+1 <= i-1) {
-                        if(palindrome[j+1][i-1] == 0) continue;
-                    }
-                    palindrome[j][i] = 1;
-                    answer = Math.max(answer, i-j+1);
-                }
+                if(s.charAt(i) != s.charAt(j)) continue;
+                if(Math.abs(i - j) > 1 && palindrome[i-1][j+1] != 1) continue;
+                palindrome[i][j] = 1;
+                answer = Math.max(answer, i-j+1);
             }
         }
-
+        
         return answer;
     }
 }
