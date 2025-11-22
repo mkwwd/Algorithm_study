e10fc485ee34f0d8cf8773afcb2f26680935e47a
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-11-22 18:07:27 +0900

    [level 2] Title: 하노이의 탑, Time: 5.45 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -8,26 +8,30 @@ class Solution {
         
         hanoi(n, 1, 2, 3);
         
-        int[][] answer = new int[list.size()][2];
+        int answer[][] = new int[list.size()][2];
         
         for(int i=0; i<list.size(); i++){
-            answer[i][0] = list.get(i)[0];
-            answer[i][1] = list.get(i)[1];
+            answer[i] = list.get(i);
         }
         
         return answer;
     }
     
-    public void hanoi(int n, int from, int by, int to){
-        if(n == 1){
+    public void hanoi(int n, int from, int ex, int to){
+        
+        if(n==1) {
             list.add(new int[]{from, to});
             return;
         }
-        // n-1개 1에서 2로 보내기
-        hanoi(n-1, from, to, by);
-        // n번째 1에서 3으로 보내기 
-        list.add(new int[] {from, to});
-        // 2에 있는거 3으로 보내기
-        hanoi(n-1, by, from, to);
-    }
+        
+        // 맨 마지막 원반을 제외한 원반을 2번으로
+        hanoi(n-1, from, to, ex);
+        // 맨 마지막 원반을 3번으로
+        list.add(new int[]{from, to});
+        // 2번에 있는 원반들을 3번으로
+        hanoi(n-1, ex, from, to);
+        
+    };
+    
+  
 }
