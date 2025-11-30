53eeac48157509f099fbaa4d63a7730c79909cd8
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-12-01 01:40:03 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 29.63 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -5,35 +5,31 @@ class Solution {
         int max = 300000;
         
         while(min <= max){
-            int mid = (min + max)/2;
-            boolean isPossible = solvePuzzle(mid, diffs, times, limit);
-            if(isPossible){
+            int mid = (min+max)/2;
+            if(isSolved(diffs, times, mid, limit)){
                 max = mid -1;
             }else{
                 min = mid + 1;
             }
-            
         }
         
         return min;
     }
     
-    public static boolean solvePuzzle(int mid, int[] diffs, int[] times, long limit){
+    public boolean isSolved(int[] diffs, int[] times, int mid, long limit){
         
         long sum = 0;
+        
         for(int i=0; i<diffs.length; i++){
-            if(mid >= diffs[i]){
+            if(diffs[i] <= mid){
                 sum += times[i];
             }else{
-                int gap = diffs[i] - mid;
-                sum += (times[i-1]+times[i])*gap + times[i];
+                sum += (times[i-1] + times[i])*(diffs[i] - mid) + times[i];
             }
-            
-            if(sum > limit) return false;
         }
         
-        return true;
-        
-        
+        if(sum <= limit) return true;
+        return false;
     }
+
 }
