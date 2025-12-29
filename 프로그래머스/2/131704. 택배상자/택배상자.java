c240a02b1f4b9ee558465955ae6cecf5fd8fe693
mkwwd <95129758+mkwwd@users.noreply.github.com>
2025-12-29 14:01:04 +0900

    [level 2] Title: 택배상자, Time: 74.36 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -4,42 +4,29 @@ class Solution {
     public int solution(int[] order) {
         
         Deque<Integer> belt = new ArrayDeque<>();
-        Deque<Integer> sub = new ArrayDeque<>();
+        Deque<Integer> temp = new ArrayDeque<>();
         
-        for(int i=1; i<=order.length; i++){
+        for(int i=1; i<= order.length; i++){
             belt.add(i);
         }
         
         int cnt = 0;
-        boolean run = true;
         
-        while(cnt < order.length && run){
+        while(cnt < order.length){
             
-            int size = belt.size();
-            boolean find = false;
-
-            for(int i=0; i<size; i++){
-                if(belt.peek() < order[cnt]){
-                    sub.add(belt.poll());
-                }else if(belt.peek() == order[cnt]){
-                    belt.poll();
-                    find = true;
-                    break;
-                }else break;
+            while(!belt.isEmpty() && order[cnt] > belt.peek()){
+                temp.add(belt.poll());
             }
             
-            //
-            if(find){
+            if(!belt.isEmpty() && order[cnt] == belt.peek()){
                 cnt++;
-            }else{
-                if(sub.peekLast() == order[cnt]){
-                    sub.pollLast();
-                    cnt++;
-                }else{
-                    run = false;
-                }
-            }
-                
+                belt.poll();
+            }else if(!temp.isEmpty() && order[cnt] == temp. peekLast()){
+                cnt++;
+                temp.pollLast();
+            }else break;
+            
+            
         }
         
         return cnt;
