51b632771347107772fe5f73eaba9c79e74e7ebc
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-01-09 05:16:30 +0900

    [level 3] Title: 입국심사, Time: 65.04 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 42.3 MB, 시간: 345.48 ms
+메모리: 89.6 MB, 시간: 65.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 19일 04:07:54
+2026년 01월 09일 05:16:31
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,29 +3,36 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         
-        long start = 0;
-        long end = Long.MAX_VALUE / 100;
+        long min = 0;
+        long max = 0;
+        
+        for(int i=0; i<times.length; i++){
+            max = Math.max(max, times[i]);
+        }
+        
+        max *= n;
+        
         long answer = 0;
         
-        while(end >= start){
-            long mid = (start + end) / 2;
-            if(isPossible(times, mid, n)){
-                answer = mid;
-                end = mid -1;
-            }else{
-                start = mid + 1;
+        while(min <= max){
+            
+            long mid = (min + max)/2;
+            long cnt = 0;
+            
+            for(int i=0; i<times.length; i++){
+                cnt += mid/times[i];
             }
+            
+            if(cnt >= n) {
+                answer = mid;
+                max = mid -1;
+            }else{
+                min = mid + 1;
+            }
+            
         }
-        
+       
         return answer;
     }
-    
-    public boolean isPossible(int[] times, long time, int n){
-        long cnt = 0;
-        for(int i=0; i<times.length; i++){
-            cnt += time/times[i];
-        }
-        if(cnt >= n) return true;
-        else return false;
-    }
+
 }
