9d0c826f3160050b832b9e8296aabbf2cc4f9f97
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-01-19 00:25:36 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 96.50 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,79 +1,82 @@
-import java.util.*;
+import java.util.*; 
 
 class Solution {
     
-    static int N, M;
-    static int dx[] = {0, -1, 0, 1}, dy[] = {-1, 0, 1, 0};
-    static int[][] oilArea;
-    static boolean[][] visited;
-    
+    static int n, m;
+    static boolean visited[][];
+    static int ground[][];
+    static int[] dx=new int[]{0,0,-1,1};
+    static int[] dy=new int[]{-1,1,0,0};
+    static HashMap<Integer, Integer> oil = new HashMap<>();
+
     public int solution(int[][] land) {
         
-        N = land.length;
-        M = land[0].length; 
+        n = land.length;
+        m = land[0].length;
+        visited = new boolean[n][m];
+        ground = new int[n][m];
+        int index = 1;
         
-        ArrayList<Integer> oil = new ArrayList<>();
-        oil.add(0, 0);
-        oilArea = new int[N][M];
-        visited = new boolean[N][M];
-        int num = 1;
-        
-        for(int i=0; i<N; i++){
-            for(int j=0; j<M; j++){
-                if(land[i][j] == 1 && !visited[i][j]){
-                    visited[i][j] = true;
-                    oilArea[i][j] = num;
-                    int total = calculateOil(land, i, j, num);
-                    oil.add(num, total);
-                    num++;
-                }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(visited[i][j]) continue;
+                if(land[i][j] == 0) continue;
+                findOil(land, i, j, index);
+                index++;
             }
         }
-        
-        boolean check[];
         
         int max = 0;
-        for(int i=0; i<M; i++){
-            check = new boolean[num];
+        HashSet<Integer> get;
+        
+        for(int i=0; i<m; i++){
+            get = new HashSet<>();
             int sum = 0;
-            for(int j=0; j<N; j++){
-                if(oilArea[j][i] == 0) continue;
-                if(check[oilArea[j][i]]) continue;
-                sum += oil.get(oilArea[j][i]);
-                check[oilArea[j][i]] = true;
+            for(int j=0; j<n; j++){
+                if(ground[j][i] != 0){
+                    get.add(ground[j][i]);
+                }
             }
-            max = Math.max(max, sum);
-        }
+            for(int amount : get){
+                sum += oil.get(amount);
+            }
+
+            max = Math.max(sum, max);
             
+        }
         
         return max;
     }
     
-    public static int calculateOil(int[][] land, int x, int y, int num){
+    public void findOil(int[][] land, int x, int y, int index){
         
         Deque<int[]> que = new ArrayDeque<>();
-        que.add(new int[]{x, y});
-        int cnt = 1;
+        que.add(new int[] {x, y});
+        visited[x][y] = true;
+        ground[x][y] = index;
+        int sum = 1;
         
         while(!que.isEmpty()){
             
-            int[] now = que.poll();
+            int now[] = que.poll();
+            
             for(int i=0; i<4; i++){
                 int nextX = now[0] + dx[i];
                 int nextY = now[1] + dy[i];
-                if(nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
-                if(land[nextX][nextY] == 0) continue;
+                if(nextX<0 || nextX>=n || nextY<0 || nextY>=m) continue;
                 if(visited[nextX][nextY]) continue;
-                visited[nextX][nextY] = true;
-                oilArea[nextX][nextY] = num;
-                cnt++;
+                if(land[nextX][nextY] == 0) continue;
                 que.add(new int[]{nextX, nextY});
+                visited[nextX][nextY] = true;
+                ground[nextX][nextY] = index;
+                sum++;
             }
             
         }
         
-        return cnt;
+        oil.put(index, sum);
         
     }
     
-}
+    
+} 
