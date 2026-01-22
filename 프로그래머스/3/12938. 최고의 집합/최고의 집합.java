637865d7869fb1828fdaf9ee3747da73386fd7f1
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-01-23 02:51:01 +0900

    [level 3] Title: 최고의 집합, Time: 0.30 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        
+        Deque<Integer> set = new ArrayDeque<>();
+        
+        int div = s/n;
+        int left = s%n;
+        
+        if(div == 0) return new int[] {-1};
+        
+        int answer[] = new int[n];
+        
+        for(int i=n-1; i>=0; i--){
+            if(left > 0){
+                answer[i] = div+1;
+                left--;
+            }else{
+                answer[i] = div;
+            }
+        }
+
+        return answer;
+    }
+}
