3e9e2d580587821c0c265c527bb90aafdb8894c8
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-02-06 12:40:57 +0900

    [level 3] Title: 아방가르드 타일링, Time: 6.62 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/181186. 아방가르드 타일링/아방가르드 타일링.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        
+        int MOD = 1000000007;
+        
+        long dp[] = new long[100001];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 10;
+        dp[4] = 23;
+        dp[5] = 62;
+        dp[6] = 170;
+        
+        for(int i=7; i<=n; i++){
+            dp[i] = (dp[i-1] + (2*dp[i-2])%MOD + (6*dp[i-3])%MOD + dp[i-4] - dp[i-6] + MOD) %MOD;
+        }
+
+        return (int)dp[n]%MOD;
+    }
+}
