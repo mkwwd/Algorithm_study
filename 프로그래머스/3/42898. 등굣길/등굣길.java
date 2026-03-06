4540e3ffc9dfda05369172f47b396c14bc0252c8
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-03-06 12:42:58 +0900

    [level 3] Title: 등굣길, Time: 0.84 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -3,26 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         
-        int[][] map = new int[n+1][m+1];
-        boolean[][] visited = new boolean[n+1][m+1];
-        int div = 1000000007;
+        int map[][] = new int[n+1][m+1];
+        boolean isPuddle[][] = new boolean[n+1][m+1];
         
         for(int i=0; i<puddles.length; i++){
-            int x = puddles[i][1];
-            int y = puddles[i][0];
-            visited[x][y] = true;
+            isPuddle[puddles[i][1]][puddles[i][0]] = true;
         }
         
         map[1][1] = 1;
         
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                if(visited[i][j]) continue;
-                map[i][j] += map[i][j-1] + map[i-1][j];
-                map[i][j] %= div;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(isPuddle[i][j]) continue;
+                map[i][j] += map[i-1][j]+map[i][j-1];
+                map[i][j] %= 1000000007;
             }
         }
-        
+       
         return map[n][m];
             
     }
