b0d74a9346039b1a8276e6ab4ae8037c088094c3
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-03-09 14:10:31 +0900

    [level 4] Title: 도둑질, Time: 51.91 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=javascript) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 29.76 ms
+메모리: 73.4 MB, 시간: 51.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 09일 14:03:40
+2026년 03월 09일 14:10:29
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.js
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -1,7 +1,9 @@
 function solution(money) {
     
-    var getFirst = new Array(money.length).fill(0);
-    var getLast = new Array(money.legnth).fill(0);
+    const len = money.length;
+    
+    const getFirst = Array(len).fill(0);
+    const getLast = Array(len).fill(0);
     
     getFirst[0] = money[0];
     getFirst[1] = Math.max(money[0], money[1]);
@@ -9,9 +11,9 @@ function solution(money) {
     getLast[0] = money[1];
     getLast[1] = Math.max(money[1], money[2]);
     
-    for(let i=2; i<money.length-1; i++){
-        getFirst[i] = Math.max(money[i]+getFirst[i-2], getFirst[i-1]);
-        getLast[i] = Math.max(money[i+1]+getLast[i-2], getLast[i-1]);
+    for(let i=2; i<len-1; i++){
+        getFirst[i] = Math.max(getFirst[i-1], money[i] + getFirst[i-2]);
+        getLast[i] = Math.max(getLast[i-1], money[i+1] + getLast[i-2]);
     }
 
     return Math.max(getFirst[money.length-2], getLast[money.length-2]);
