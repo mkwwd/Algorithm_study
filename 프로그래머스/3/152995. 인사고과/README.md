21ec9eb230ae464cb5655a2075798e715e14d0b9
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-03-20 01:33:48 +0900

    [level 3] Title: 인사고과, Time: 1489.30 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/README.md
+++ 프로그래머스/3/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [level 3] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995?language=java) 
 
 ### 성능 요약
 
-메모리: 55.7 MB, 시간: 5117.93 ms
+메모리: 110 MB, 시간: 1489.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 06일 05:23:27
+2026년 03월 20일 01:33:45
 
 ### 문제 설명
 
--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -5,40 +5,35 @@ class Solution {
         
         int a = scores[0][0];
         int b = scores[0][1];
-        int sum = a+b;
         
         Arrays.sort(scores, new Comparator<int[]>(){
             @Override
             public int compare(int o1[], int o2[]){
-                return (o2[0] + o2[1]) -  (o1[0] + o1[1]);
+                return (o2[0]+o2[1]) - (o1[0]+o1[1]);
             }
-        }); 
+        });
         
         int cnt = 0;
         
         for(int i=0; i<scores.length; i++){
-            // 
-            boolean possible = true;
-            for(int j=i-1; j>=0; j--){
-                if(scores[i][0] < scores[j][0] && scores[i][1] < scores[j][1]){
-                    possible = false;
+            if(scores[i][0] + scores[i][1] < a+b) break;
+            boolean isPoss = true;
+            for(int j=0; j<i; j++){
+                if(scores[j][0] > scores[i][0] && scores[j][1] > scores[i][1]){
+                    isPoss = false;
                     break;
                 }
             }
-            
-            if(possible && scores[i][0] + scores[i][1] > sum) {
-                System.out.println(i);
+            if(isPoss && scores[i][0] + scores[i][1] > a+b){
                 cnt++;
-            }
-            
-            if(scores[i][0] == a && scores[i][1] == b) {
-                if(possible) cnt++;
-                else cnt = -1;
+            }else if(scores[i][0] == a && scores[i][1] == b){
+                if(!isPoss) cnt = -1;
+                else cnt++;
                 break;
-            };
-            
+            }
         }
         
+       
         return cnt;
     }
 }
