c2125f4c77e19881db6f3d3bd4f301fb9d982bfd
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-03-20 01:50:50 +0900

    [level 3] Title: 인사고과, Time: 3545.59 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/README.md
+++ 프로그래머스/3/152995. 인사고과/README.md
@@ -1,10 +1,10 @@
 # [level 3] 인사고과 - 152995 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/152995?language=javascript) 
 
 ### 성능 요약
 
-메모리: 110 MB, 시간: 1489.30 ms
+메모리: 55.3 MB, 시간: 3545.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 20일 01:33:45
+2026년 03월 20일 01:50:47
 
 ### 문제 설명
 
--- 프로그래머스/3/152995. 인사고과/인사고과.js
+++ 프로그래머스/3/152995. 인사고과/인사고과.js
@@ -2,31 +2,30 @@ function solution(scores) {
     
     let a = scores[0][0];
     let b = scores[0][1];
-    let sum = a+b;
     
-    scores.sort((a, b) => (b[0]+b[1]) -(a[0]+a[1]))
+    scores.sort((a,b) => (b[0]+b[1])-(a[0]+a[1]));
     
-    var answer = 0;
+    let answer = 0;
     
     for(let i=0; i<scores.length; i++){
-        var possible = true;
-        for(let j=i-1; j>=0; j--){
-            if(scores[i][0] < scores[j][0] && scores[i][1] < scores[j][1]){
-                possible = false;
-                console.log("Dd")
+        if(scores[i][0] + scores[i][1] < a + b) break;
+        let isPoss = true;
+        for(let j=0; j<i; j++){
+            if(scores[j][0] > scores[i][0] && scores[j][1] > scores[i][1]){
+                isPoss = false;
                 break;
             }
         }
         
-        if(possible && scores[i][0] + scores[i][1] > sum) answer++;
-        
-        if(scores[i][0] == a && scores[i][1] == b){
-            if(possible) answer++
-            else answer = -1
+        if(isPoss && scores[i][0] + scores[i][1] > a + b){
+            answer++;
+        }else if(scores[i][0] == a && scores[i][1] == b){
+            if(!isPoss) answer = -1;
+            else answer++;
             break;
         }
-        
     }
+   
     
     return answer;
 }
