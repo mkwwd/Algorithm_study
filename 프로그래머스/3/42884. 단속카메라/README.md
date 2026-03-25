7df04cda0f6cc27278a00ee706129f95d8e20799
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-03-25 23:13:21 +0900

    [level 3] Title: 단속카메라, Time: 20.55 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=javascript) 
 
 ### 성능 요약
 
-메모리: 58.2 MB, 시간: 9.22 ms
+메모리: 35.4 MB, 시간: 20.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 25일 23:05:26
+2026년 03월 25일 23:13:18
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,19 +1,17 @@
 function solution(routes) {
     
-    routes.sort((a, b) => {
-        if(a[1] == b[1]){
-            return a[0] - b[0]
-        }else return a[1] - b[1]
-    })
+    var answer = 0;
     
-    var cctv = routes[0][1];
-    var cnt = 1;
+    routes.sort((a, b) => a[1] - b[1]);
+    
+    var end = -50000;
     
     for(let i=0; i<routes.length; i++){
-        if(routes[i][0] <= cctv && cctv <= routes[i][1]) continue
-        cnt++
-        cctv = routes[i][1]
+        if(end < routes[i][0]){
+            end = routes[i][1];
+            answer++;
+        }
     }
     
-    return cnt;
+    return answer;
 }
