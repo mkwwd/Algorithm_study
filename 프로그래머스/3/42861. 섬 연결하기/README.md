81fd509e5fe11dd07f3efeb18c281e0cef6fc7b3
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-03-28 00:40:42 +0900

    [level 3] Title: 섬 연결하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=javascript) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 0.72 ms
+메모리: 33.4 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 27일 23:41:15
+2026년 03월 28일 00:40:45
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -3,30 +3,32 @@ const parent = [];
 
 function solution(n, costs) {
     
-    costs.sort((a,b) => a[2] - b[2])
+    costs.sort((a, b) => a[2]-b[2]);
     
-    for(let i=0; i<n; i++){
-        parent.push(i)
+    for(let i=0; i<n+1; i++){
+        parent.push(i);
     }
     
     var answer = 0;
     
     for(let i=0; i<costs.length; i++){
-        if(find(costs[i][0]) == find(costs[i][1])) continue;
-        answer += costs[i][2]
-        union(costs[i][0], costs[i][1])
+        var from = costs[i][0];
+        var to = costs[i][1];
+        if(find(from) == find(to)) continue;
+        answer += costs[i][2];
+        union(from, to);
     }
     
-    return answer
+    return answer;
 }
 
-function union(a, b){
-    let pa = parent[a]
-    let pb = parent[b]
-    if(pa != pb) parent[pb] = pa
+function find(node){
+    if(parent[node] == node) return node;
+    return parent[node] = find(parent[node]);
 }
 
-function find(x){
-    if(parent[x] == x) return x
-    else return parent[x] = find(parent[x]);
+function union(a , b){
+    let pa = parent[a];
+    let pb = parent[b];
+    if(pa != pb) parent[pb] = pa;
 }
