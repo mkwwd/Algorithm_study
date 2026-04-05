60bfbc372c73422a42d0b66bd36ccb3df2237647
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-04-05 23:25:22 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.60 ms, Memory: 46 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=javascript) 
 
 ### 성능 요약
 
-메모리: 116 MB, 시간: 30.47 ms
+메모리: 46 MB, 시간: 44.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 23:18:02
+2026년 04월 05일 23:25:25
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -2,29 +2,30 @@ function solution(n, edge) {
     
     const node = Array.from({length : n+1}, () => []);
     
-    edge.map(([from, to]) => {
-        node[from].push(to)
-        node[to].push(from)
-    })
-    
-    const que = [1];
-    const visited = Array(n+1).fill(false);
-    visited[1] = true;
-    let size = que.length;
-    
-    while(que.length > 0){
-        size = que.length
-        for(let i=0; i<size; i++){
-            const now = que.shift()
-            for(let next of node[now]){
-                if(visited[next]) continue
-                visited[next] = true
-                que.push(next)
-            }
-        }
-        
+    for(let i=0; i<edge.length; i++){
+        let from = edge[i][0];
+        let to = edge[i][1];
+        node[from].push(to);
+        node[to].push(from);
     }
     
+    const visited = Array(n+1).fill(false);
+    visited[1] = true;
+    
+    const que = [1];
+    var size;
+    
+    while(que.length){
+        var size = que.length;
+        for(let i=0; i<size; i++){
+            var now = que.shift();
+            for(let next of node[now]){
+                if(visited[next]) continue;
+                visited[next] = true;
+                que.push(next);
+            }
+        }
+    }
     
     return size;
 }
