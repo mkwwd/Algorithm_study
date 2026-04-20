7270d8d8501c63c2888a9bf10352c2f4199fc17a
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-04-20 18:08:59 +0900

    [level 3] Title: 순위, Time: 11.59 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=java) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 14.32 ms
+메모리: 73.7 MB, 시간: 11.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 13일 16:42:54
+2026년 04월 20일 18:08:56
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -4,50 +4,33 @@ class Solution {
     public int solution(int n, int[][] results) {
         
         int player[][] = new int[n+1][n+1];
+        
         for(int i=0; i<results.length; i++){
             int win = results[i][0];
             int lose = results[i][1];
             player[win][lose] = 1;
         }
         
+        for(int k=1; k<n+1; k++){
+            for(int i=1; i<n+1; i++){
+                for(int j=1; j<n+1; j++){
+                    if(player[i][k] == 1 && player[k][j] == 1){
+                        player[i][j] = 1;
+                    }
+                }
+            }
+        }
+        
         int answer = 0;
         
         for(int i=1; i<n+1; i++){
-            Deque<Integer> que = new ArrayDeque<>();
-            que.add(i);
-            boolean check[] = new boolean[n+1];
-            check[i] = true;
             int cnt = 0;
-            while(!que.isEmpty()){
-                int now = que.poll();
-                for(int j=1; j<n+1; j++){
-                    if(player[now][j] == 0) continue;
-                    if(check[j]) continue;
-                    check[j] = true;
-                    que.add(j);
+            for(int j=1; j<n+1; j++){
+                if(player[i][j] == 1 || player[j][i] == 1){
                     cnt++;
                 }
             }
-            
-            que.add(i);
-            check = new boolean[n+1];
-            check[i] = true;
-            
-            while(!que.isEmpty()){
-                int now = que.poll();
-                for(int j=1; j<n+1; j++){
-                    if(player[j][now] == 0) continue;
-                    if(check[j]) continue;
-                    check[j] = true;
-                    que.add(j);
-                    cnt++;
-                }
-            }
-            
-            if(cnt == n-1){
-                answer++;
-            }
-
+            if(cnt == n-1) answer++;
         }
         
         return answer;
