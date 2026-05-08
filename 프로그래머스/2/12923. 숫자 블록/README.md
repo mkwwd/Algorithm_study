940f2eca264fae506e108148f47998b48e107715
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-05-08 14:46:18 +0900

    [level 2] Title: 숫자 블록, Time: 57.32 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/README.md
+++ 프로그래머스/2/12923. 숫자 블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 블록 - 12923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12923?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12923?language=java) 
 
 ### 성능 요약
 
-메모리: 37.3 MB, 시간: 143.43 ms
+메모리: 57.1 MB, 시간: 57.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 28일 23:29:33
+2026년 05월 08일 14:46:13
 
 ### 문제 설명
 
--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -2,28 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(long begin, long end) {
-        int size = (int)(end-begin+1);
-        int[] answer = new int[size];
+        
+        int size = (int)(end - begin + 1);
+        int answer[] = new int[size];
         
         for(int i=0; i<size; i++){
-            long num = begin + i;
-            answer[i] = find(num);
+            answer[i] = find((int)(i + begin));
         }
         
         return answer;
     }
     
-    public int find(long now){
-        
-        if(now == 1) return 0;
+    public int find(int num){
+        if(num == 1) return 0;
         int max = 1;
-        for(long i = 2; i<=Math.sqrt(now); i++){
-            if(now%i == 0){
-                max = (int)i;
-                long ans = now/i;
-                if(ans <= 10000000) return (int)ans;
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num % i == 0){
+                max = i;
+                int ans = num/max;
+                if(ans <= 10000000) return ans;
             }
         }
+        
         return max;
     }
+    
 }
