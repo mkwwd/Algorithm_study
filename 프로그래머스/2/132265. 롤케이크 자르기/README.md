abb47fe615726bc78afc2c2233a7f2d92818db24
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-05-14 23:39:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 143.31 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java) 
 
 ### 성능 요약
 
-메모리: 71.4 MB, 시간: 143.82 ms
+메모리: 156 MB, 시간: 143.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 06일 03:12:24
+2026년 05월 14일 23:39:02
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,28 +3,26 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         
-        HashMap<Integer, Integer> a = new HashMap<>();
-        HashMap<Integer, Integer> b = new HashMap<>();
+        HashMap<Integer, Integer> chulsoo = new HashMap<>();
+        HashMap<Integer, Integer> brother = new HashMap<>();
         
         for(int i=0; i<topping.length; i++){
-            b.put(topping[i], b.getOrDefault(topping[i], 0) + 1);
+            brother.put(topping[i], brother.getOrDefault(topping[i], 0)+1);
         }
         
-        int cnt = 0;
+        int answer = 0;
         
         for(int i=0; i<topping.length; i++){
-            a.put(topping[i], a.getOrDefault(topping[i], 0) + 1);
-            int out = b.getOrDefault(topping[i], 0);
-            if(out == 1){
-                b.remove(topping[i]);
+            chulsoo.put(topping[i], chulsoo.getOrDefault(topping[i], 0)+1);
+            if(brother.get(topping[i]) == 1){
+                brother.remove(topping[i]);
             }else{
-                b.put(topping[i], out-1);
+                brother.put(topping[i], brother.get(topping[i])-1);
             }
-            
-            if(a.size() == b.size()) cnt++;
+            if(chulsoo.size() == brother.size()) answer++;
         }
         
-        return cnt;
+        return answer;
         
     }
 }
