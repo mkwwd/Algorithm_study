001a3f827c0ea3033df181e4e0287a0c86dd274c
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-05-22 04:06:21 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.14 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/README.md
+++ 프로그래머스/2/12936. 줄 서는 방법/README.md
@@ -1,10 +1,10 @@
 # [level 2] 줄 서는 방법 - 12936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12936?language=javascript) 
 
 ### 성능 요약
 
-메모리: 56.1 MB, 시간: 0.06 ms
+메모리: 32.2 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 22일 03:54:37
+2026년 05월 22일 04:06:19
 
 ### 문제 설명
 
--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,25 +1,27 @@
 function solution(n, k) {
     
-    var fact = 1;
-    var number = [];
+    var permu = 1;
+    const people = [];
     
     for(let i=1; i<=n; i++){
-        fact *= i;
-        number.push(i);
+        permu *= i;
+        people.push(i);
     }
     
     var answer = [];
     
-    while(n > 1){
-        fact /= n;
-        let find = Math.ceil(k / fact)-1;
-        k -= fact*(find)
-        n--
-        answer.push(number[find])
-        number.splice(find,1)
+    while(people.length){
+        if(people.length == 1){
+            answer.push(people.pop());
+            break;
+        }
+        
+        permu /= people.length;
+        var num = Math.floor((k-1)/permu);
+        answer.push(people[num]);
+        k -= num*permu;
+        people.splice(num,1);
     }
-    
-    answer.push(number[0])
     
     return answer;
 }
