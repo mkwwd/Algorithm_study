55f5dd6aa512cdbb5a3ff95dfadca13c28b1a18e
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-06-01 14:45:37 +0900

    [level 2] Title: 숫자의 표현, Time: 0.32 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=java) 
 
 ### 성능 요약
 
-메모리: 36.3 MB, 시간: 1.53 ms
+메모리: 58.7 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 01일 14:09:59
+2026년 06월 01일 14:45:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,21 +2,23 @@ class Solution {
     public int solution(int n) {
         
         int left = 1;
-        int right = 1;
-        int sum = 1;
+        int right = 2;
+        int sum = left+right;
         int answer = 0;
+        
+        if(n <= 2) return 1;
         
         while(left <= right){
             if(sum <= n){
-                if(sum == n) answer++;                
+                if(sum == n) answer++;
                 right++;
-                sum+=right;
+                sum += right;
             }else{
                 sum -= left;
                 left++;
             }
         }
-        
+       
         return answer;
     }
 }
