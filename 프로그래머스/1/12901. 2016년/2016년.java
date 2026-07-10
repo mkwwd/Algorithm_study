598b377fdabfc6411feb922df6de7ceff777c26b
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-07-10 14:26:20 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int a, int b) {
+        
+        String day[] = { "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int month[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        int sum = 0;
+        
+        for(int i=0; i<a-1; i++){
+            sum += month[i];
+        }
+        
+        sum += b;
+        
+        int findDay = sum%7;
+        
+        String answer = day[findDay];
+        
+        return answer;
+    }
+}
