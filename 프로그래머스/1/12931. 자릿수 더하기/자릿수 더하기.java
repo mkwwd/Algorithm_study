a24d857137e3d871595a16c97cc37d0be2b9ea4e
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-08-03 15:00:24 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int answer = 0;
+
+        while(n > 0){
+            int mod = n%10;
+            answer += mod;
+            n /= 10;
+        }  
+
+        return answer;
+    }
+}
