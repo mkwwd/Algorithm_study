5f0ca86e110f84e67f9ec58cfcc5d3249eb1f707
mkwwd <95129758+mkwwd@users.noreply.github.com>
2026-09-11 15:56:52 +0900

    [level 2] Title: 124 나라의 숫자, Time: 2.10 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,17 +1,15 @@
 class Solution {
     public String solution(int n) {
         
-        String rule[] = {"4", "1", "2"};
+        String word[] = {"4", "1", "2"};
         String answer = "";
         
         while(n > 0){
-            
-            int div = n%3;
-            answer = rule[div] + answer;
+            int mod = n%3;
+            answer = word[mod]+answer;
             n /= 3;
-            if(div == 0) n--;
+            if(mod==0) n--;
         }
-        
         
         return answer;                                                                
     }
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899?language=java) 
 
 ### 성능 요약
 
-메모리: 32.9 MB, 시간: 0.17 ms
+메모리: 86 MB, 시간: 2.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 31일 23:01:26
+2026년 09월 11일 15:56:51
 
 ### 문제 설명
 
